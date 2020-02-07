c8bfcb8d083a67c1e81c13996316f575f81dc1ad
onlie6731 <mohammad.parsian@gmail.com>
2020-02-07 23:04:31 +0330

    QuickUnionImprovedUF class and networkx testing added
    - QuickUnionImprovedUF class added as the implementation of improved version of quick-union algorithms
    - graph plotting and algorithm result testing added using networkx module

--- union-find.py
+++ union-find.py
@@ -1,7 +1,27 @@
+from random import randint
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
 class BaseUF():
     """
     base class for implementation of union-find classes
     """
+
+    @staticmethod
+    def input_to_list(lines):
+        """converts input lines to a list of lists containing integers
+
+            for example :
+                lines:
+                    "2 3 4
+                    5 9
+                    5 0
+                    9 4"
+                output:
+                    [[2,3,4], [5,9], [5,0], [9,4]]
+            """
+        return list(map(lambda line: list(map(int, line.split())), lines.split('\n')))
 
     @staticmethod
     def test(_class, input_text=None, debug=True):
@@ -9,26 +29,43 @@ class BaseUF():
         run and test the class with given method and input
         """
 
-        from random import randint
-
         # if no input has given, input_text fills out randomly
         if input_text == None:
-            input_text = '\n'.join(['10 5 5'] + [f'{randint(0, 9)} {randint(0, 9)}' for i in range(5)] +  [f'{randint(0, 9)} {randint(0, 9)}' for i in range(5)])
+            input_text = '\n'.join(
+                ['10 8 8'] +
+                [f'{randint(0, 9)} {randint(0, 9)}' for i in range(8)] +
+                [f'{randint(0, 9)} {randint(0, 9)}' for i in range(8)]
+            )
 
-        lines = input_text.split('\n')
-        nodes_n, unions_n, queries_n = map(int, lines[0].split())
-        union_queries = map(lambda line: list(map(int, line.split())), lines[1:unions_n+1])
-        connected_queries = map(lambda line: list(map(int, line.split())), lines[unions_n+1:unions_n+queries_n+1])
+        lines = BaseUF.input_to_list(input_text)
+        nodes_n, unions_n, queries_n = lines[0]
+        union_queries = lines[1:unions_n+1]
+        connected_queries = lines[unions_n+1:unions_n+queries_n+1]
         result = ''
 
-        uf = _class(nodes_n)
-        for p,q in union_queries:
-            uf.union(p,q)
-            if debug: print(uf.ids, f'- {p} and {q} connected')
+        G = nx.Graph()
+        G.add_nodes_from(range(nodes_n))
 
-        for p,q in connected_queries:
+        uf = _class(nodes_n)
+        for p, q in union_queries:
+            uf.union(p, q)
+            G.add_edge(p, q, weight=1)
+            if debug:
+                print(uf.ids, f'- {p} and {q} connected')
+
+        for p, q in connected_queries:
             result += f'{uf.connected(p,q)}\n'
-            if debug: print(f'{p} and {q}:', 'connected' if uf.connected(p,q) else 'not connected')
+            if debug:
+                print(f'Are {p} and {q} connected ? ', 'YES' if uf.connected(
+                    p, q) else 'NO ', ' - ', 'right' if nx.has_path(G, p, q) == uf.connected(p, q) else 'wrong', 'answer',
+                    '(checked by networkx module)')
+
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_nodes(G, pos, node_size=700)
+        nx.draw_networkx_edges(G, pos, edgelist=G.edges(), width=6)
+        nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
+        plt.axis('off')
+        plt.show()
 
         return result
 
@@ -47,7 +84,6 @@ class QuickFindUF(BaseUF):
 
         self.ids = [i for i in range(N)]
 
-
     def union(self, p, q):
         """
         connect the two given elements
@@ -56,7 +92,6 @@ class QuickFindUF(BaseUF):
         """
 
         self.ids = [i if i != self.ids[p] else self.ids[q] for i in self.ids]
-
 
     def connected(self, p, q):
         """
@@ -85,7 +120,6 @@ class QuickUnionUF(BaseUF):
 
         self.ids = [i for i in range(N)]
 
-
     def union(self, p, q):
         """
         connect the two given elements
@@ -95,7 +129,6 @@ class QuickUnionUF(BaseUF):
 
         self.ids[self.root(p)] = self.ids[q]
 
-
     def connected(self, p, q):
         """
         return True if two given elements are connected, otherwise False
@@ -104,7 +137,6 @@ class QuickUnionUF(BaseUF):
         """
 
         return self.root(p) == self.root(q)
-
 
     def root(self, p):
         """
@@ -121,6 +153,36 @@ class QuickUnionUF(BaseUF):
         BaseUF.test(QuickFindUF, input_text=input_text, debug=debug)
 
 
+class QuickUnionImprovedUF(BaseUF):
+
+    ids = []
+    weights = []
+
+    def __init__(self, N):
+        self.ids = [i for i in range(N)]
+        self.weights = [1 for i in range(N)]
+
+    def root(self, p):
+        t = p
+        while t != self.ids[t]:
+            t = self.ids[t]
+        return t
+
+    def union(self, p, q):
+        # swaping p and q if the weight of root of q is more than p
+        if self.weights[self.root(p)] > self.weights[self.root(q)]:
+            p, q = q, p
+        self.ids[self.root(q)] = self.root(p)
+        self.weights[self.root(p)] += self.weights[self.root(q)]
+
+    def connected(self, p, q):
+        return self.root(p) == self.root(q)
+
+    def test(input_text=None, debug=True):
+        BaseUF.test(QuickUnionImprovedUF, input_text=input_text, debug=debug)
+
+
 if __name__ == '__main__':
-    QuickFindUF.test(debug=True)
-    QuickUnionUF.test(debug=True)
+    # QuickFindUF.test(debug=True)
+    # QuickUnionUF.test(debug=True)
+    QuickUnionImprovedUF.test(debug=True)
